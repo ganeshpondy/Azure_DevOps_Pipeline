cf4ab8f9f7b5b7c09e2b3499c5ad707ad7afe241
root <root@DESKTOP-6RBK9MQ.localdomain>
2022-05-18 06:52:01 +0530

    updated_Correct_Client_ID & VMSize

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "terraform-k8s" {
     name            = "agentpool"
     node_count      = var.node_count
     #vm_size         = "standard_b2ms"
-    vm_size         = "standard_d4ads_v5"
+    vm_size         = "standard_d2ads_v5"
   }
 
   service_principal {
