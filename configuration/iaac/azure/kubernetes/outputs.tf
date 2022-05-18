e162d15b01510b735ceab9fcfe04f930ef3f78ee
root <root@DESKTOP-6RBK9MQ.localdomain>
2022-05-18 07:50:47 +0530

    updated_outputtf

--- configuration/iaac/azure/kubernetes/outputs.tf
+++ configuration/iaac/azure/kubernetes/outputs.tf
@@ -1,13 +1,16 @@
 output "client_key" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
 
 output "cluster_username" {
@@ -16,6 +19,7 @@ output "cluster_username" {
 
 output "cluster_password" {
   value = azurerm_kubernetes_cluster.terraform-k8s.kube_config.0.password
+  sensitive = true
 }
 
 output "kube_config" {
